61581b09d938840ddb3f370daf2fe7361fe13f13
otaviob <otaviob22@gmail.com>
2024-12-20 17:22:46 +1100

    Initial Commit

---
+++ iOS-Features-NetworkIntegrationUITests/iOS_Features_NetworkIntegrationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOS_Features_NetworkIntegrationUITestsLaunchTests.swift
+//  iOS-Features-NetworkIntegrationUITests
+//
+//  Created by Otavio Brito on 20/12/2024.
+//
+
+import XCTest
+
+final class iOS_Features_NetworkIntegrationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
